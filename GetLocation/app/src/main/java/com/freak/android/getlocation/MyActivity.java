68a24ddea7d8c37d3a27fbfb0d4f8f7a1f08d916
Pilou44 <guillaume.beguin@gmail.com>
2014-12-03 15:32:47 +0100

    [GetLocation][DEV] Add button to launch maps

--- GetLocation/app/src/main/java/com/freak/android/getlocation/MyActivity.java
+++ GetLocation/app/src/main/java/com/freak/android/getlocation/MyActivity.java
@@ -1,38 +1,34 @@
 package com.freak.android.getlocation;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MyActivity extends Activity {
 
     public static final String PREFERENCE_NAME = "asteroid_location";
     private TextView mText;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
         mText = (TextView) findViewById(R.id.text);
+        mButton = (Button) findViewById(R.id.go_maps);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        SharedPreferences pref = getSharedPreferences(PREFERENCE_NAME, 0);
-        Double latitude = Double.longBitsToDouble(pref.getLong("latitude", Double.doubleToLongBits(0.0)));
-        Double longitude = Double.longBitsToDouble(pref.getLong("longitude", Double.doubleToLongBits(0.0)));
-        Double accuracy = Double.longBitsToDouble(pref.getLong("accuracy", Double.doubleToLongBits(0.0)));
-
-        mText.setText("Latitude = " + latitude + "\nLongitude = " + longitude + "\nAccuracy = " + accuracy);
-
-        //Intent newIntent = new Intent(this, MyService.class);
-        //this.startService(newIntent);
-
+        refresh();
     }
 
     @Override
@@ -47,10 +43,28 @@ public class MyActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        /*int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            refresh();
             return true;
-        }*/
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void refresh() {
+        SharedPreferences pref = getSharedPreferences(PREFERENCE_NAME, 0);
+        final Double latitude = Double.longBitsToDouble(pref.getLong("latitude", Double.doubleToLongBits(0.0)));
+        final Double longitude = Double.longBitsToDouble(pref.getLong("longitude", Double.doubleToLongBits(0.0)));
+        final Double accuracy = Double.longBitsToDouble(pref.getLong("accuracy", Double.doubleToLongBits(0.0)));
+
+        mText.setText("Latitude = " + latitude + "\nLongitude = " + longitude + "\nAccuracy = " + accuracy);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("geo:0,0?q=" + latitude + "," + longitude + " (" + MyActivity.this.getString(R.string.my_car) + ")"));
+                startActivity(intent);
+            }
+        });
     }
 }
