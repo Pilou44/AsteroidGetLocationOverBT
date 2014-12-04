33164aa687d5e8746d7841f22943251b6cd42fdf
Pilou44 <guillaume.beguin@gmail.com>
2014-12-04 16:01:27 +0100

    [LocationOverBt][DEV] Start service only when a device is connected

--- LocationOverBt/app/src/main/java/com/freak/fidji/locationoverbt/MyBootReceiver.java
+++ LocationOverBt/app/src/main/java/com/freak/fidji/locationoverbt/MyBootReceiver.java
@@ -1,13 +1,20 @@
 package com.freak.fidji.locationoverbt;
 
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.util.Log;
 
 public class MyBootReceiver extends BroadcastReceiver {
     private static final String TAG = "BOOT_RECEIVER";
     private static final boolean DEBUG = true;
+    private String mAction;
+    private Context mContext;
+    private BluetoothDevice mDevice;
 
     public MyBootReceiver() {
     }
@@ -15,9 +22,35 @@ public class MyBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (DEBUG)
-            Log.i(TAG, "Boot received");
+            Log.i(TAG, "Intent received");
 
+        mContext = context;
         Intent newIntent = new Intent(context, MyService.class);
-        context.startService(newIntent);
+        mContext.startService(newIntent);
+        mAction = intent.getAction();
+        mDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+        mContext.bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
     }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            if (DEBUG)
+                Log.i(TAG, "Service connected");
+
+            MyService myService = ((MyService.LocalBinder)service).getService();
+            if (mAction.equals(BluetoothDevice.ACTION_ACL_CONNECTED)){
+                myService.startThread(mDevice);
+            }
+            else if (mAction.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED)){
+                myService.stopThread(mDevice);
+            }
+            mContext.unbindService(mConnection);
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            if (DEBUG)
+                Log.i(TAG, "Service disconnected");
+        }
+    };
+
 }
--- LocationOverBt/app/src/main/java/com/freak/fidji/locationoverbt/MyService.java
+++ LocationOverBt/app/src/main/java/com/freak/fidji/locationoverbt/MyService.java
@@ -1,12 +1,9 @@
 package com.freak.fidji.locationoverbt;
 
 import android.app.Service;
-import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -17,15 +14,20 @@ public class MyService extends Service implements SendingThreadListener{
     private static final String TAG = "LOCATION_OVER_BT";
     private static final boolean DEBUG = true;
 
+    private final IBinder mBinder = new LocalBinder();
+
     private Vector<BluetoothDevice> mConnectedDevices;
     private Vector<SendingThread> mSendingThreads;
 
-    public MyService() {
+    public class LocalBinder extends Binder {
+        MyService getService() {
+            return MyService.this;
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 
     @Override
@@ -37,66 +39,33 @@ public class MyService extends Service implements SendingThreadListener{
 
         mConnectedDevices = new Vector<BluetoothDevice>();
         mSendingThreads = new Vector<SendingThread>();
-
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter != null) {
-
-            if (DEBUG)
-                Log.d(TAG, "Instantiate filters");
-            IntentFilter filter1 = new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED);
-            IntentFilter filter2 = new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED);
-            IntentFilter filter3 = new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-
-            if (DEBUG)
-                Log.d(TAG, "Register receivers");
-            this.registerReceiver(mReceiver, filter1);
-            this.registerReceiver(mReceiver, filter2);
-            this.registerReceiver(mReceiver, filter3);
-
-        }
     }
 
-    @Override
-    public void onDestroy() {
+    public void startThread(BluetoothDevice device) {
         if (DEBUG)
-            Log.d(TAG, "Unregister receiver");
-        this.unregisterReceiver(mReceiver);
-        super.onDestroy();
+            Log.d(TAG, "Device connected");
+        mConnectedDevices.add(device);
+        SendingThread thread = new SendingThread(this, device);
+        thread.setListener(MyService.this);
+
+        if (DEBUG)
+            Log.d(TAG, "Start thread for device " + (mConnectedDevices.size() - 1));
+        thread.start();
+        mSendingThreads.add(thread);
     }
 
-    //The BroadcastReceiver that listens for bluetooth broadcasts
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-            if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+    public void stopThread(BluetoothDevice device) {
+        if (DEBUG)
+            Log.d(TAG, "Device disconnected, try to identify");
+        for (int i = 0 ; i < mConnectedDevices.size() ; i++){
+            if (mConnectedDevices.get(i).getAddress().equals(device.getAddress())) {
                 if (DEBUG)
-                    Log.d(TAG, "Device connected");
-                mConnectedDevices.add(device);
-                SendingThread thread = new SendingThread(context, device);
-                thread.setListener(MyService.this);
-
-                if (DEBUG)
-                    Log.d(TAG, "Start thread for device " + (mConnectedDevices.size() - 1));
-                thread.start();
-                mSendingThreads.add(thread);
-            }
-            else if ((BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED.equals(action)) || (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action))) {
-                if (DEBUG)
-                    Log.d(TAG, "Device disconnected, try to identify");
-                for (int i = 0 ; i < mConnectedDevices.size() ; i++){
-                    if (mConnectedDevices.get(i).getAddress().equals(device.getAddress())) {
-                        if (DEBUG)
-                            Log.d(TAG, "Device identified, try to stop thread");
-                        mSendingThreads.get(i).disconnect();
-                        break;
-                    }
-                }
+                    Log.d(TAG, "Device identified, try to stop thread");
+                mSendingThreads.get(i).disconnect();
+                break;
             }
         }
-    };
+    }
 
     @Override
     public void onThreadFinished(BluetoothDevice device) {
