266d8f7f1ff53f8de5dbd98a2c0afb83f534a198
Pilou44 <guillaume.beguin@gmail.com>
2014-12-05 11:17:57 +0100

    [LocationOverBt][DEV] Add ManagerService to bind MyService as Receiver can't bind a service

---
+++ LocationOverBt/app/src/main/java/com/freak/fidji/locationoverbt/ManagerService.java
@@ -0,0 +1,65 @@
+package com.freak.fidji.locationoverbt;
+
+import android.app.IntentService;
+import android.bluetooth.BluetoothDevice;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.Context;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.Log;
+
+public class ManagerService extends IntentService {
+
+    private static final boolean DEBUG = true;
+    private static final String TAG = "MANAGER_SERVICE";
+
+    private String mState;
+    private BluetoothDevice mDevice;
+
+    public ManagerService() {
+        super("ManagerService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (DEBUG)
+            Log.d(TAG, "Create service, handle intent");
+        if (intent != null) {
+            mState = intent.getStringExtra("CONNEXION_STATE");
+            mDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+            Intent serviceIntent = new Intent(this, MyService.class);
+            this.startService(serviceIntent);
+            this.bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        }
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            if (DEBUG)
+                Log.d(TAG, "Service connected");
+
+            MyService myService = ((MyService.LocalBinder)service).getService();
+            if (mState.equals(BluetoothDevice.ACTION_ACL_CONNECTED)){
+                myService.startThread(mDevice);
+            }
+            else if (mState.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED)){
+                myService.stopThread(mDevice);
+            }
+            ManagerService.this.unbindService(mConnection);
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            if (DEBUG)
+                Log.d(TAG, "Service disconnected");
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG)
+            Log.d(TAG, "Destroy service");
+        super.onDestroy();
+    }
+}
--- LocationOverBt/app/src/main/java/com/freak/fidji/locationoverbt/MyBootReceiver.java
+++ LocationOverBt/app/src/main/java/com/freak/fidji/locationoverbt/MyBootReceiver.java
@@ -12,9 +12,6 @@ import android.util.Log;
 public class MyBootReceiver extends BroadcastReceiver {
     private static final String TAG = "BOOT_RECEIVER";
     private static final boolean DEBUG = true;
-    private String mAction;
-    private Context mContext;
-    private BluetoothDevice mDevice;
 
     public MyBootReceiver() {
     }
@@ -22,35 +19,12 @@ public class MyBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (DEBUG)
-            Log.i(TAG, "Intent received");
+            Log.d(TAG, "Intent received");
 
-        mContext = context;
-        Intent newIntent = new Intent(context, MyService.class);
-        mContext.startService(newIntent);
-        mAction = intent.getAction();
-        mDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-        mContext.bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        Intent newIntent = new Intent(context, ManagerService.class);
+        newIntent.putExtra(BluetoothDevice.EXTRA_DEVICE, intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE));
+        newIntent.putExtra("CONNEXION_STATE", intent.getAction());
+        context.startService(newIntent);
     }
 
-    private ServiceConnection mConnection = new ServiceConnection() {
-        public void onServiceConnected(ComponentName className, IBinder service) {
-            if (DEBUG)
-                Log.i(TAG, "Service connected");
-
-            MyService myService = ((MyService.LocalBinder)service).getService();
-            if (mAction.equals(BluetoothDevice.ACTION_ACL_CONNECTED)){
-                myService.startThread(mDevice);
-            }
-            else if (mAction.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED)){
-                myService.stopThread(mDevice);
-            }
-            mContext.unbindService(mConnection);
-        }
-
-        public void onServiceDisconnected(ComponentName className) {
-            if (DEBUG)
-                Log.i(TAG, "Service disconnected");
-        }
-    };
-
-}
+ }
